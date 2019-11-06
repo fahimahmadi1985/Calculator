db0dcee942c1662bab0f305d5f60df0d2a5284ad
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 02:31:59 +0100

    Updates

---
+++ precache-manifest.d39dbff835bb9eaee8b57248180988e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445e9516d9bfa2b591c68d99982924cc",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "8c5f499d39b4dc167973",
+    "url": "/Calculator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "917a50622c1c75e2c253",
+    "url": "/Calculator/static/js/2.2c725533.chunk.js"
+  },
+  {
+    "revision": "8c5f499d39b4dc167973",
+    "url": "/Calculator/static/js/main.a0e9a43a.chunk.js"
+  },
+  {
+    "revision": "0ced635b02481fa1dcc4",
+    "url": "/Calculator/static/js/runtime-main.4d854def.js"
+  }
+]);
